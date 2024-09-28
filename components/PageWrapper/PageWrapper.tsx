003ae9f11f2ba3babc128ad5b6f44a13c83b82f3
pavel-liashkevich <paul8paul8@mail.ru>
2024-09-28 15:11:19 +0300

    test: import

--- components/PageWrapper/PageWrapper.tsx
+++ components/PageWrapper/PageWrapper.tsx
@@ -1,5 +1,5 @@
-import { PropsWithChildren } from "react";
 import styled from "styled-components";
+import { PropsWithChildren } from "react";
 import {HeadMeta} from '../HeadMeta/HeadMeta';
 
 type PropsType = {
