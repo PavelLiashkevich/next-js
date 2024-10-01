df7aeb004d6e902aba750db53de211c522306128
pavel <paul8paul8@mail.ru>
2024-10-01 10:12:55 +0300

    feat: add get static props

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -1,20 +1,35 @@
-import { useQuery } from "@tanstack/react-query"
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query"
 import { LocationType, ResponseType } from "../../assets/api/rick-and-morty-api"
 import { Header } from "../../components/Header/Header"
 import { PageWrapper } from "../../components/PageWrapper/PageWrapper"
 
 const getLocations = () => {
-   return fetch('https://rickandmortyapi.com/api/location', {
-    method: 'GET' 
-   }).then(res => res.json())
+  return fetch("https://rickandmortyapi.com/api/location", {
+    method: "GET",
+  }).then((res) => res.json())
+}
+
+export const getStaticProps = async () => {
+  const queryClient = new QueryClient()
+
+  await queryClient.fetchQuery(["locations"], getLocations)
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
 }
 
 const Locations = () => {
-  const { data: locations } = useQuery<ResponseType<LocationType>>(['locations'], getLocations)
+  const { data: locations } = useQuery<ResponseType<LocationType>>(
+    ["locations"],
+    getLocations
+  )
 
   if (!locations) {
-        return null
-      }
+    return null
+  }
 
   const locationsList = locations.results.map((location) => (
     <div key={location.id}>{location.name}</div>
