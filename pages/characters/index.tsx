78bc5af7a3aac060f27bf818842f1ce54eda8bcd
pavel <paul8paul8@mail.ru>
2024-11-07 17:48:42 +0300

    feat: add lazy imports

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,31 +1,34 @@
-import { API } from "assets/api/api"
-import { CharacterType, ResponseType } from "assets/api/rick-and-morty-api"
-import { CharacterCard } from "components/Card/CharacterCard/CharacterCard"
-import { PageWrapper } from "components/PageWrapper/PageWrapper"
-import { getLayout } from "components/Layout/BaseLayout/BaseLayout"
+import {API} from "assets/api/api"
+import {CharacterType, ResponseType} from "assets/api/rick-and-morty-api"
+import {PageWrapper} from "components/PageWrapper/PageWrapper"
+import {getLayout} from "components/Layout/BaseLayout/BaseLayout"
+import dynamic from "next/dynamic";
+
+const CharacterCard = dynamic(() => import('components/Card/CharacterCard/CharacterCard')
+    .then(module => module.CharacterCard))
 
 export const getStaticProps = async () => {
-  const characters = await API.rickAndMorty.getCharacters()
-  return {
-    props: {
-      characters,
-    },
-    revalidate: 60,
-  }
+    const characters = await API.rickAndMorty.getCharacters()
+    return {
+        props: {
+            characters,
+        },
+        revalidate: 60,
+    }
 }
 
 type PropsType = {
-  characters: ResponseType<CharacterType>
+    characters: ResponseType<CharacterType>
 }
 
 const Characters = (props: PropsType) => {
-  const { characters } = props
+    const {characters} = props
 
-  const charactersList = characters.results.map((character) => (
-    <CharacterCard key={character.id} character={character} />
-  ))
+    const charactersList = characters.results.map((character) => (
+        <CharacterCard key={character.id} character={character}/>
+    ))
 
-  return <PageWrapper title="Characters">{charactersList}</PageWrapper>
+    return <PageWrapper title="Characters">{charactersList}</PageWrapper>
 }
 
 Characters.getLayout = getLayout
